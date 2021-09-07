4e0c003330c9ef53a360aecd16c0a1adec64c96f
Jake <jakemiller13@hotmail.com>
2021-09-07 11:04:02 -0400

    Adds setup and distribution files (setup.py, requirements.txt)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'unclePy',
+    version = '1.0',
+    packages = ['unclePy'],
+    url = 'https://github.com/eric-hunt/unclePy',
+    license = 'GNU Affero General Public License v3.0',
+    author = 'Jacob Miller',
+    author_email = 'jmiller@neb.com',
+    description = 'A parser for Unchained Labs Uncle HDF5 binary files written in unclePy. '
+)
